f32411eb9dd5e3169d6f63a1e08edf9cc6354717
Rodrigo Casio <rod.casio@outlook.com>
2025-05-27 20:39:27 -0600

    Update AddTwoIntegersEasy.cpp

--- AddTwoIntegersEasy.cpp
+++ AddTwoIntegersEasy.cpp
@@ -1,6 +1,18 @@
+#include <iostream>
+
 class Solution {
 public:
     int sum(int num1, int num2) {
         return num1 + num2;
     }
 };
+
+int main(){
+
+    Solution obj1;
+    
+    int result = obj1.sum(3, 4);
+    std::cout << result << std::endl;
+    return 0;
+}
+
